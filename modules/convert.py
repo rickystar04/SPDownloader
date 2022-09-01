2dbebe562467034657db2d443c9022d58583a830
rickystar04 <70718835+rickystar04@users.noreply.github.com>
2022-09-01 09:15:42 -0400

    Update convert.py

--- modules/convert.py
+++ modules/convert.py
@@ -1,15 +1,8 @@
 import moviepy.editor as mp
 import os
-from time import sleep
 
 def extract_audio(video_path, playlist_name, savename):
-	clip=mp.VideoFileClip(video_path)
-	clip.audio.write_audiofile("music/"+playlist_name+"/"+savename)
+	clip=mp.VideoFileClip(video_path, verbose=False)
+	clip.audio.write_audiofile("music/"+playlist_name+"/"+savename, logger=None)
 	clip.close()
 	os.remove(video_path)
-	print("Done")
-
-
-def path(video_path, playlist_name, savename):
-	extract_audio(video_path, playlist_name, savename)
-
