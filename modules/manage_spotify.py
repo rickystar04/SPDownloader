715b1e02ae42d5596a0535011f1bee6cd51fdf3f
rickystar04 <70718835+rickystar04@users.noreply.github.com>
2022-09-01 09:15:58 -0400

    Update manage_spotify.py

--- modules/manage_spotify.py
+++ modules/manage_spotify.py
@@ -1,14 +1,13 @@
-
 import cred
 from spotify import *
 
 if(cred.client_ID=="" and cred.client_SECRET==""):
-	print("Client ID e  Client Secret non presenti")
-	print("Segui le istruzioni presenti nel file README.md per ottenere le tue credenziali")
+	print("Client ID and Client Secret not present")
+	print("Follow the instructions in the README.md file to obtain your credentials")
 elif (cred.client_ID==""):
-	print("Client ID non presente")
-	print("Segui le istruzioni presenti nel file README.md per ottenere le tue credenziali")
+	print("Client ID not present")
+	print("Follow the instructions in the README.md file to obtain your credentials")
 	
 elif(cred.client_SECRET==""):
-	print("Client Secret non presente")
-	print("Segui le istruzioni presenti nel file README.md per ottenere le tue credenziali")
+	print("Client Secret not present")
+	print("Follow the instructions in the README.md file to obtain your credentials")
