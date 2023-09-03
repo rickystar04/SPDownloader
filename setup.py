c5dcc5b4cb306e42d0ab596932dbd105a5511a1a
rickystar04 <70718835+rickystar04@users.noreply.github.com>
2023-09-03 14:11:04 +0200

    New update

---
+++ setup.py
@@ -0,0 +1,14 @@
+import os
+
+os.system("pip install spotipy")
+os.system("pip install moviepy")
+os.system("pip install eyed3")
+os.system("pip install pytube")
+os.system("pip install ffmpeg")
+os.system("pip install tqdm")
+os.system("pip install pandas")
+os.system("pip install requests")
+os.system("pip install spotipy")
+os.system("pip install mutagen")
+os.system("pip install flask")
+os.system("pip install flask_session")
